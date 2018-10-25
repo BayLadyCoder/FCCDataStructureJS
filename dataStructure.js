909067ea76895d125bd93edef50980b4125f2333
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-10-24 22:28:58 -0400

    Array with push() and unshift()

--- dataStructure.js
+++ dataStructure.js
@@ -67,3 +67,32 @@ let myArray = ["a", "b", "c", "d"];
 myArray[1] = "Yayy!"
 //change code above this line
 console.log(myArray);
+
+
+/*
+ the push() method adds elements to the end of an array, and unshift() adds elements to the beginning. Consider the following:
+
+let twentyThree = 'XXIII';
+let romanNumerals = ['XXI', 'XXII'];
+
+romanNumerals.unshift('XIX', 'XX');
+// now equals ['XIX', 'XX', 'XXI', 'XXII']
+
+romanNumerals.push(twentyThree);
+// now equals ['XIX', 'XX', 'XXI', 'XXII', 'XXIII']
+Notice that we can also pass variables, which allows us even greater flexibility in dynamically modifying our array's data.
+
+
+We have defined a function, mixedNumbers, which we are passing an array as an argument. Modify the function by using push() and unshift() to add 'I', 2, 'three' to the beginning of the array and 7, 'VIII', 9 to the end so that the returned array contains representations of the numbers 1-9 in order.
+*/
+
+function mixedNumbers(arr) {
+  // change code below this line
+  arr.push(7, 'VIII', 9);
+  arr.unshift('I', 2, 'three');
+  // change code above this line
+  return arr;
+}
+
+// do not change code below this line
+console.log(mixedNumbers(['IV', 5, 'six']));
